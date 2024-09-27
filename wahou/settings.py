d7c743b50cf6a200e8ba315447b59786a2477d82
Your GitHub Username <Your GitHub Email>
2024-09-27 16:57:31 +0200

    share the images folder into github

--- wahou/settings.py
+++ wahou/settings.py
@@ -14,9 +14,6 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 # Load environment variables from .env file
 load_dotenv()
 
